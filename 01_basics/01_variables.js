6742b6aa3fb0e573c07102a2d87b3f95652137a0
Arpit Kumar Shukla <110964036+shuklaarpit005@users.noreply.github.com>
2024-03-19 06:18:52 +0000

    know about variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 258013;
+let accountEmail = "arpitshukla18@google.com";
+var accountPassword = "123546";
+accountCity = "Delhi";
+
+// accountId=2;//constant variables are not allowed to reinitialise
+
+accountEmail = "google1@google.com";
+accountPassword = "124578";
+accountCity = "Noida";
+
+// console.log(accountEmail);
+
+// var does not know what scope is, hence it is preferred not to use var keywork for variable initilisation.
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+
