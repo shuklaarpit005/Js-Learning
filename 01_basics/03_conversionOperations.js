b4d6dd2f785aa8a3901f249823b14ff6bb281001
Arpit Kumar Shukla <110964036+shuklaarpit005@users.noreply.github.com>
2024-03-19 07:41:47 +0000

    tricky conversions

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -37,3 +37,37 @@ console.log(typeof stringNumber);
 true/false => "true"/"false"
 undefined and null => "undefined" and "null"
 */
+
+//link to study Primitive Conversion
+/*https://tc39.es/ecma262/#sec-toprimitive*/
+
+//################### OPERATIONS ####################
+
+console.log(2+2);//4
+console.log(2-2);//0
+console.log(2*3);//6
+console.log(2/2);//1
+console.log(2/2);//1
+console.log(4%2);//0
+console.log(2**3);//8
+
+let str1="hello"
+let str2=" Arpit"
+console.log(str1+str2);// hello Arpit
+
+console.log("1"+2);//12
+console.log(1+"2");//12
+console.log("1"+2+2);//122
+console.log(1+2+"2");//32
+/*
+it first check the first value type if string then later values are returned as string.
+if number then checks the later as number or string then perform operations.
+*/
+console.log(( 3 + 3 ) * 5 % 3 );//0 ((9*5)%3)
+/*
+here operation are done from left to right giving priority to braces.
+*/
+
+console.log(+true);//1
+console.log(+"");//0
+
