6aa2142a5f66bd99380092ffd92d6295a11f8795
Arpit Kumar Shukla <110964036+shuklaarpit005@users.noreply.github.com>
2024-03-19 09:35:30 +0000

    summary of datatypes.

---
+++ datatypes-summary.js
@@ -0,0 +1,37 @@
+/*
+Primitive datatypes
+
+7 types:
+String, number, boolean, null, undefined, Symbol, BigInt.
+*/
+
+const naam="Arpit" // string
+const score=100; //number
+const scoreValue=110.3; //number
+
+const isLoggedIn=true; // boolean
+const outsideTemp=null; // null
+let userEmail; //undefined
+
+const id=Symbol('123'); // Symbol
+
+const bigNumber=48451498745166878n; //bigInt
+
+/*
+
+Refernce Types(Non primitive):
+Array, Object, functions.
+*/
+
+let arr=["Ironman","Hanuman", "Superman"];// Array
+
+let meObj={    // Object
+    name:"Arpit",
+    age:23
+}
+
+const fun=function(){   //function
+    console.log("hello arpit");
+}
+
+
